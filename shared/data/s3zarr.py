ad7f4ff6179cd7e9ca2850b03781b5839d5a40bf
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-18 18:52:50 -0400

    Initial Setup

--- shared/data/s3zarr.py
+++ shared/data/s3zarr.py
@@ -1,10 +1,9 @@
 import boto3
-# from boto3.resources.base import ServiceResource
 
 s3 = boto3.resource('s3')
 
 for bucket in s3.buckets.all():
     if bucket.name.startswith("eis"):
         print(f'{bucket.name}:')
-        for obj in bucket.objects:
+        for obj in bucket.objects.all():
             print(f'{obj.key}: {obj.__class__}')
