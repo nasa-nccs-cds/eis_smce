399bac8bb1cc35afdd413da2d0f3b49277892fcf
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-18 19:01:02 -0400

    Initial Setup

--- shared/data/s3zarr.py
+++ shared/data/s3zarr.py
@@ -1,15 +1,18 @@
 import boto3
 
 itemname = 'mod14/raw/MOD14.A2020296.0645.061.2020348134049.hdf'
+bucketname = 'eis-dh-fire'
 
 s3 = boto3.resource('s3')
 
-for bucket in s3.buckets.all():
-    if bucket.name.startswith("eis"):
-        print(f'{bucket.name}:')
+# for bucket in s3.buckets.all():
+#    if bucket.name.startswith("eis"):
+#        print(f'{bucket.name}:')
 #        for obj in bucket.objects.all():
- #           print(f'{obj.key}: {obj.__class__}')
+#           print(f'{obj.key}: {obj.__class__}')
 
 
-#obj = s3.Object(bucketname, itemname)
-#body = obj.get()['Body'].read()
+obj = s3.Object(bucketname, itemname)
+body = obj.get()['Body'].read()
+
+print( f"Body: {body.__class__}")
