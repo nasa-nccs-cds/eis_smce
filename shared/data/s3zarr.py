82ae0da6a9f62874b0587da38dac6ee1bd416ae1
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-19 12:33:35 -0400

    Add s3fs to s3zarr

--- shared/data/s3zarr.py
+++ shared/data/s3zarr.py
@@ -1,5 +1,6 @@
 import boto3
 import io, h5py
+import s3fs
 import xarray as xr
 
 itemname = 'mod14/raw/MOD14.A2020296.0645.061.2020348134049.hdf'
@@ -14,10 +15,14 @@ s3 = boto3.resource('s3')
 #           print(f'{obj.key}: {obj.__class__}')
 
 
-obj = s3.Object(bucketname, itemname)
-hdf_bytes = obj.get()['Body'].read()
-f = io.BytesIO(hdf_bytes)
-h = h5py.File(f,'r')
+# obj = s3.Object(bucketname, itemname)
+# hdf_bytes = obj.get()['Body'].read()
+# f = io.BytesIO(hdf_bytes)
+# h = h5py.File(f,'r')
 # ds: xr.Dataset = xr.open_dataset( f, engine='h5py' )
+
+
+s3 = s3fs.S3FileSystem()
+f = h5py.File(s3.open("s3://my-bucket/my-file.h5", "rb"))
 
 print( f"READ ds:")
