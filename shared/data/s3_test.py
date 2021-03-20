1c3336fc7ee7faea8d86fa3d17b0ceaa7c418322
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-20 14:31:34 -0400

    Add s3fs to s3zarr

--- shared/data/s3_test.py
+++ shared/data/s3_test.py
@@ -1,15 +1,20 @@
 import boto3
 
+default_bucketname = 'eis-dh-fire'
 dev_bucketname = 'eis-dh-fire-dev'
 
 s3 = boto3.client('s3')
 response = s3.list_buckets()
 
-print( "Current eis buckets:")
+print( "\nCurrent eis buckets:")
 for bucket in response['Buckets']:
     print(f'  {bucket["Name"]}')
 
-print( f"\nCreating bucket: {dev_bucketname}")
+print( f"\nBucket policy for {default_bucketname}:")
+result = s3.get_bucket_policy(Bucket=default_bucketname)
+print(result['Policy'])
+
+print( f"\nCreating bucket: {dev_bucketname}\n")
 s3.create_bucket(Bucket=dev_bucketname)
 
 print( "DONE")
