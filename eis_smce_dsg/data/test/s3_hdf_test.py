b45c0bfed33887d39ba019b1b34f413e17800be5
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-21 18:52:57 -0400

    Add s3fs to s3zarr

---
+++ eis_smce_dsg/data/test/s3_hdf_test.py
@@ -0,0 +1,16 @@
+import boto3
+from pyhdf.SD import SD, SDC, SDS
+import os
+
+modis_s3_item = 'mod14/raw/MOD14.A2020296.0645.061.2020348134049.hdf'
+bucketname = 'eis-dh-fire'
+local_cache_dir = "/home/jovyan/cache"
+client = boto3.client('s3')
+
+file_name = modis_s3_item.split("/")[-1]
+modis_filepath = os.path.join( local_cache_dir, file_name )
+client.download_file( bucketname, modis_s3_item, modis_filepath )
+
+sd = SD( modis_filepath, SDC.READ )
+for (akey, aval) in sd.attributes().items():
+    print(f"{akey} = {aval}")
