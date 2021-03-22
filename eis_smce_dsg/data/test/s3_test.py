33bc2ff56c13170f216a48646fc0c6d3f4c91e6c
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-22 16:08:22 -0400

    Update test

--- eis_smce_dsg/data/test/s3_test.py
+++ eis_smce_dsg/data/test/s3_test.py
@@ -3,10 +3,11 @@ import boto3
 bucketname = 'eis-dh-fire'
 s3 = boto3.client('s3')
 
-response = s3.list_buckets()
-print( "\nCurrent eis buckets:")
-for bucket in response['Buckets']:
-    print(f'  {bucket["Name"]}')
+for bucket in s3.buckets.all():
+   if bucket.name.startswith("eis"):
+       print(f'{bucket.name}:')
+       for obj in bucket.objects.all():
+          print(f'{obj.key}: {obj.__class__}')
 
 print( f"\nUploading script to bucket: {bucketname}\n")
 response = s3.upload_file( __file__, bucketname, 'mod14/raw/test_script.py' )
