abfed8134131589474d9f06dffd95ba007b432b7
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-23 17:11:24 -0400

    Update test

--- eis_smce_dsg/data/test/s3_hdf_test.py
+++ eis_smce_dsg/data/test/s3_hdf_test.py
@@ -2,14 +2,14 @@ import boto3
 from pyhdf.SD import SD, SDC, SDS
 import os
 
-modis_s3_item = 'mod14/raw/MOD14.A2020296.0645.061.2020348134049.hdf'
+modis_s3_item = 'mod14/raw/MOD14.A2020298.1835.061.2020348153757.hdf'
 bucketname = 'eis-dh-fire'
 local_cache_dir = "/home/jovyan/cache"
 client = boto3.client('s3')
 
 file_name = modis_s3_item.split("/")[-1]
 modis_filepath = os.path.join( local_cache_dir, file_name )
-# client.download_file( bucketname, modis_s3_item, modis_filepath )
+if not os.path.exists(modis_filepath): client.download_file( bucketname, modis_s3_item, modis_filepath )
 print( f"Reading file {modis_filepath}")
 
 sd = SD( modis_filepath, SDC.READ )
