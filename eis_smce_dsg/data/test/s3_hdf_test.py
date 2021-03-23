cc1a6a06ec6c6ce17b2ebf491a3203b8096b130d
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-23 17:24:18 -0400

    Update test

--- eis_smce_dsg/data/test/s3_hdf_test.py
+++ eis_smce_dsg/data/test/s3_hdf_test.py
@@ -13,24 +13,29 @@ if not os.path.exists(modis_filepath): client.download_file( bucketname, modis_s
 print( f"Reading file {modis_filepath}")
 
 sd = SD( modis_filepath, SDC.READ )
+dsets = sd.datasets().keys()
 print( f"METADATA keys = {sd.attributes().keys()} ")
-print( f"DATASET keys = {sd.datasets().keys()} ")
+# print( f"DATASET keys = {sd.datasets().keys()} ")
+#
+# DATAFIELD_NAME='FP_T31'
+# sds: SDS = sd.select(DATAFIELD_NAME)
+# print( f"FP_T31 dimensions = {sds.dimensions()}" )
+# print( f"FP_T31 attributes = {sds.attributes().keys()}" )
+#
+# sample: SDS = sd.select('FP_sample')
+# print( f"sample data = {sample[:]}" )
+#
+# line: SDS = sd.select('FP_line')
+# print( f"line data = {line[:]}" )
+#
+# # Read geolocation dataset.
+# lat_sds: SDS = sd.select('FP_latitude')
+# print( f"latitude dimensions = {lat_sds.dimensions()}" )
+# print( f"latitude attributes = {lat_sds.attributes().keys()}" )
+#
+# lon_sds: SDS = sd.select('FP_longitude')
+# print( f"longitude dimensions = {lon_sds.dimensions()}" )
 
-DATAFIELD_NAME='FP_T31'
-sds: SDS = sd.select(DATAFIELD_NAME)
-print( f"FP_T31 dimensions = {sds.dimensions()}" )
-print( f"FP_T31 attributes = {sds.attributes().keys()}" )
-
-sample: SDS = sd.select('FP_sample')
-print( f"sample data = {sample[:]}" )
-
-line: SDS = sd.select('FP_line')
-print( f"line data = {line[:]}" )
-
-# Read geolocation dataset.
-lat_sds: SDS = sd.select('FP_latitude')
-print( f"latitude dimensions = {lat_sds.dimensions()}" )
-print( f"latitude attributes = {lat_sds.attributes().keys()}" )
-
-lon_sds: SDS = sd.select('FP_longitude')
-print( f"longitude dimensions = {lon_sds.dimensions()}" )
+print( f"DIMENSIONS:")
+for dset in dsets:
+    print( f" {dset}: {sd.select(dset).dimensions()}")
