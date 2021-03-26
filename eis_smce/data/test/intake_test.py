f0fd806d5ff6e8593a955f06bcc097292a8b4876
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-26 14:30:01 -0400

    Update drivers

--- eis_smce/data/test/intake_test.py
+++ eis_smce/data/test/intake_test.py
@@ -2,7 +2,7 @@ import intake
 from eis_smce.data.intake.hdf4.drivers import HDF4Source
 from intake_xarray.xzarr import ZarrSource
 
-item_key = 'mod14/raw/MOD14.A2020296.0645.061.2020348134049'
+item_key = 'mod14/raw/MOD14.A2020298.1835.061.2020348153757'
 bucketname = 'eis-dh-fire'
 
 ds: HDF4Source = intake.open_hdf4( f"s3://{bucketname}/{item_key}.hdf" )
