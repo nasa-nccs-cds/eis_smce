aa329ca302e6ca75cefa0b1f17a7d4cc548b8ebc
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-26 17:59:50 -0400

    Update drivers

--- eis_smce/data/test/rioxarray_test.py
+++ eis_smce/data/test/rioxarray_test.py
@@ -1,9 +1,14 @@
 import rioxarray as rxr
 from xarray.core.dataset import Dataset
+from xarray.core.variable import Variable
 from typing import List, Union, Dict, Callable, Tuple, Optional, Any, Type, Mapping, Hashable
 
 cache_file = "/home/jovyan/.eis_smce/cache/MOD14.A2020298.1835.061.2020348153757.hdf"
 modis_pre: List[Dataset] = rxr.open_rasterio( cache_file )
 
-for dset in modis_pre:
-    print( f"{ dset.items() }" )
+ds0: Dataset = modis_pre[0]
+print( f"DS-0 attributes:\n {ds0.attrs}")
+print( f"DS-0 variables:\n {ds0.variables}")
+for vid, v in ds0.variables.items():
+    print(f"  {vid}{v.dims} ({v.shape})")
+
