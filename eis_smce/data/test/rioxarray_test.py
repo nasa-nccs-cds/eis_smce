6609b6511b00c3d65b9532b5bd00c454d03ca99e
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-26 17:46:10 -0400

    Update drivers

--- eis_smce/data/test/rioxarray_test.py
+++ eis_smce/data/test/rioxarray_test.py
@@ -1,8 +1,8 @@
 import rioxarray as rxr
-
+from typing import List, Union, Dict, Callable, Tuple, Optional, Any, Type, Mapping, Hashable
 
 cache_file = "/home/jovyan/.eis_smce/cache/MOD14.A2020298.1835.061.2020348153757.hdf"
+modis_pre: List = rxr.open_rasterio( cache_file )
 
-modis_pre = rxr.open_rasterio(cache_file)
-
-print( type(modis_pre) )
+for item in modis_pre:
+    print( str( item.__class__ ) )
