1c2b3dc348bae35682f1961af5f82969a53b1c56
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-27 18:53:48 -0400

    Update drivers

--- eis_smce/data/test/s3_test.py
+++ eis_smce/data/test/s3_test.py
@@ -1,13 +1,25 @@
 import boto3
+from intake.source.utils import reverse_format
 
 bucketname = 'eis-dh-fire'
 s3 = boto3.resource('s3')
+pattern = "mod14/raw/MOD14.{date}.{tod}.061.{ts}.hdf"
 
+files_list = []
 for bucket in s3.buckets.filter( Bucket= bucketname ):
     if bucket.name == bucketname:
         print(f'** {bucket.name}:')
         for obj in bucket.objects.all():
-            print(f'   -> {obj.key}: {obj.__class__}')
+            try:
+                metadata = reverse_format( pattern, obj.key )
+                metadata['resolved'] = obj.key
+                files_list.append( metadata )
+            except ValueError: pass
+
+
+for entry in files_list:
+    print( entry )
+
 
 
 # for bucket in s3.buckets.all():
