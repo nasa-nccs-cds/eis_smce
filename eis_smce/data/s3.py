befe6406d7fac1c3c24dc516190cfc204aae9d7f
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-27 19:26:32 -0400

    Update drivers

--- eis_smce/data/s3.py
+++ eis_smce/data/s3.py
@@ -1,5 +1,10 @@
 import traitlets.config as tlc
+import fnmatch
+from typing import List, Union, Dict, Callable, Tuple, Optional, Any, Type, Mapping, Hashable
+import glob
 import boto3, s3fs
+
+def s3m(): return S3Manager.instance()
 
 class S3Manager(tlc.SingletonConfigurable):
 
@@ -24,3 +29,24 @@ class S3Manager(tlc.SingletonConfigurable):
         s3f: s3fs.S3FileSystem  = s3fs.S3FileSystem( anon=True )
         store = s3fs.S3Map( root=f"{bucketname}/{s3path}/{modis_filename}_test1", s3=s3f, check=False, create=True )
         return store
+
+    def get_file_list(self, bucketname: str, pattern: str ) -> List[Dict]:
+        from intake.source.utils import reverse_format
+        def has_char(string: str, chars: str): return 1 in [c in string for c in chars]
+        s3 = boto3.resource('s3')
+        is_glob = has_char( pattern, "*?[" )
+        files_list = []
+        for bucket in s3.buckets.all():
+            if fnmatch.fnmatch(bucket.name,bucketname):
+                for obj in bucket.objects.all():
+                    if is_glob:
+                        if fnmatch.fnmatch( obj.key, pattern ):
+                            files_list.append( {'resolved': obj.key} )
+                    else:
+                        try:
+                            metadata = reverse_format(pattern, obj.key)
+                            metadata['resolved'] = obj.key
+                            files_list.append(metadata)
+                        except ValueError:
+                            pass
+        return files_list
--- eis_smce/data/test/s3_test.py
+++ eis_smce/data/test/s3_test.py
@@ -1,34 +1,10 @@
-import boto3
-from intake.source.utils import reverse_format
+from eis_smce.data.s3 import s3m
 
 bucketname = 'eis-dh-fire'
-s3 = boto3.resource('s3')
-pattern = "mod14/raw/MOD14.{date}.{tod}.061.{ts}.hdf"
+pattern = "mod14/raw/MOD14.{id0}.{id1}.061.{id2}.hdf"
 
-files_list = []
-for bucket in s3.buckets.filter( Bucket= bucketname ):
-    if bucket.name == bucketname:
-        print(f'** {bucket.name}:')
-        for obj in bucket.objects.all():
-            try:
-                metadata = reverse_format( pattern, obj.key )
-                metadata['resolved'] = obj.key
-                files_list.append( metadata )
-            except ValueError: pass
-
+files_list = s3m().get_file_list(bucketname,pattern)
 
 for entry in files_list:
     print( entry )
 
-
-
-# for bucket in s3.buckets.all():
-#    if bucket.name.startswith("eis"):
-#        print(f'** {bucket.name}:')
-#        for obj in bucket.objects.all():
-#           print(f'   -> {obj.key}: {obj.__class__}')
-
-# print( f"\nUploading script to bucket: {bucketname}\n")
-# response = s3.upload_file( __file__, bucketname, 'mod14/raw/test_script.py' )
-#
-# print( response )
