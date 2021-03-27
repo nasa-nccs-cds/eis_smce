b70e8888c1f1ed8ac040a510cef433ad963f461a
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-27 19:29:06 -0400

    Update drivers

--- eis_smce/data/test/s3_test.py
+++ eis_smce/data/test/s3_test.py
@@ -2,8 +2,9 @@ from eis_smce.data.s3 import s3m
 
 bucketname = 'eis-dh-fire'
 pattern = "mod14/raw/MOD14.{id0}.{id1}.061.{id2}.hdf"
+glob_pattern = "mod14/raw/MOD14.*.hdf"
 
-files_list = s3m().get_file_list(bucketname,pattern)
+files_list = s3m().get_file_list( bucketname, glob_pattern )
 
 for entry in files_list:
     print( entry )
