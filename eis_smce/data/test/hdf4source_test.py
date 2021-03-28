43a5fe1f3bf6ee0354e01b62cbf9f6604b619a04
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-03-28 18:13:53 -0400

    Update drivers

--- eis_smce/data/test/hdf4source_test.py
+++ eis_smce/data/test/hdf4source_test.py
@@ -3,8 +3,8 @@ from xarray.core.variable import Variable
 from eis_smce.data.intake.hdf4.drivers import HDF4Source
 from typing import List, Union, Dict, Callable, Tuple, Optional, Any, Type, Mapping, Hashable
 
-cache_file = "/home/jovyan/.eis_smce/cache/MOD14.{sample}.hdf"
-h4s: HDF4Source = HDF4Source( cache_file )
+data_url = "s3://eis-dh-fire/mod14/raw/MOD14.{sample}.hdf"
+h4s: HDF4Source = HDF4Source( data_url )
 ds0: Dataset = h4s.read_partition(0)
 
 print(f"\n HDF4Source DATASET DS0:" )
