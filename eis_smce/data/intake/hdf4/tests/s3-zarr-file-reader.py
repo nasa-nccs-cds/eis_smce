1553cc5adcbcb88bec05c34b5fa2787fb100fb83
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-02 13:27:16 -0400

    Update drivers

--- eis_smce/data/intake/hdf4/tests/s3-zarr-file-reader.py
+++ eis_smce/data/intake/hdf4/tests/s3-zarr-file-reader.py
@@ -1,9 +1,13 @@
 from intake_xarray.xzarr import ZarrSource
+import xarray as xa
 
 urlpath = 's3://eis-dh-fire/mod14/raw/MOD14.A2020303.0515.061.2020349105630.zarr'
 
 xzSource: ZarrSource = ZarrSource( urlpath )
 
-data = xzSource.read()
+data: xa.Dataset = xzSource.read()
 
-print( data.__class__ )
+print( f"attrs: {data.attrs}" )
+
+for id, var in data.data_vars.items():
+    print( f"   {id}{var.dims}: shape={var.shape}" )
