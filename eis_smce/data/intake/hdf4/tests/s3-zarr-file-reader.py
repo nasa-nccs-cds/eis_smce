c4be5f44b5452b06ada516d9b47ff94ebaa14133
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-02 13:23:25 -0400

    Update drivers

--- eis_smce/data/intake/hdf4/tests/s3-zarr-file-reader.py
+++ eis_smce/data/intake/hdf4/tests/s3-zarr-file-reader.py
@@ -4,4 +4,6 @@ urlpath = 's3://eis-dh-fire/mod14/raw/MOD14.A2020303.0515.061.2020349105630.zarr
 
 xzSource: ZarrSource = ZarrSource( urlpath )
 
-print( xzSource.describe() )
+data = xzSource.read()
+
+print( data.__class__ )
