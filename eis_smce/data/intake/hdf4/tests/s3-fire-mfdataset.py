9d0b80ca3c6b606ba021eb58c10a70350e8e1e35
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-02 15:50:38 -0400

    Update drivers

--- eis_smce/data/intake/hdf4/tests/s3-fire-mfdataset.py
+++ eis_smce/data/intake/hdf4/tests/s3-fire-mfdataset.py
@@ -7,4 +7,4 @@ data_batch_url: str = "s3://eis-dh-fire/mod14/raw/MOD14.A2020303.{tod}.061.{samp
 h4s: HDF4Source = HDF4Source( data_batch_url  )             # Creates source encapsulating all matched files in data_url
 ds0: xr.Dataset = h4s.read()                                # Reads all partitions into lazy chunked dask array.
 
-print( ds0 )
+print( ds0.compute() )
