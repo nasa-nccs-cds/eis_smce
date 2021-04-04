6cc3e2824bba1db5ccf1070f3a6e10aca9ea5cf4
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-04 18:18:16 -0400

    Update drivers

--- eis_smce/data/intake/hdf4/tests/mfdataset.py
+++ eis_smce/data/intake/hdf4/tests/mfdataset.py
@@ -1,9 +1,17 @@
-import os, xarray as xr
 import numpy as np
 from typing import List, Union, Dict, Callable, Tuple, Optional, Any, Type, Mapping, Hashable
+import os, glob, xarray as xr
 
-data_files = [ "/Users/tpmaxwel/allData/61/MCDWD_L3_NRT/2020/292/MCDWD_L3_NRT.A2020292.h00v02.061.nc", "/Users/tpmaxwel/allData/61/MCDWD_L3_NRT/2020/292/MCDWD_L3_NRT.A2020292.h26v06.061.nc" ]
-test_var = "Water Counts 3-Day 250m"
+base_dir = "/att/nobackup/mcarrol2/MODIS"
+cache_dir = "/att/nobackup/tpmaxwel/ILAB/scratch"
+collection = "MCD12Q1"
+location = "h09v09"
+batch = f"200?/001/MCD12Q1.A200?001.{location}.006.*.hdf"
+output_file = f"{cache_dir}/{collection}/{location}.zarr"
+os.makedirs( os.path.dirname(output_file), exist_ok=True )
+data_files = glob.glob( f"{base_dir}/{collection}/{batch}" )
+
+test_var = "QC"
 file_indices = range( len(data_files) )
 
 for iF in file_indices:
