e4724a76b227ba69e17a0fec8bc7b00320cd9ac5
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-04 14:42:07 -0400

    Update adapt test

--- eis_smce/data/intake/hdf4/tests/adapt-modis-gpp.py
+++ eis_smce/data/intake/hdf4/tests/adapt-modis-gpp.py
@@ -6,14 +6,13 @@ from intake_xarray.xzarr import ZarrSource
 base_dir = "/att/nobackup/mcarrol2/MODIS"
 cache_dir = "/att/nobackup/tpmaxwel/ILAB/scratch"
 collection = "MCD12Q1"
-batch = "200?/001/MCD12Q1.A200?001.h09v09.006.*.hdf"
-file = "2001/001/MCD12Q1.A2001001.h09v09.006.2018142183530.h"
-output_dir = os.path.dirname(f"{cache_dir}/{collection}/{batch}")
-output_file = f"{cache_dir}/{collection}/{file}"
-os.makedirs( output_dir, exist_ok=True )
+location = "h09v09"
+batch = f"200?/001/MCD12Q1.A200?001.{location}.006.*.hdf"
+output_file = f"{cache_dir}/{collection}/{location}.zarr"
+os.makedirs( os.path.dirname(output_file), exist_ok=True )
 part_index: int = 0
 
-data_url = f"file:/{base_dir}/{collection}/{file}"
+data_url = f"file:/{base_dir}/{collection}/{batch}"
 h4s: HDF4Source = HDF4Source( data_url  )                              # Creates source encapsulating all matched files in data_url
 ds0: xr.Dataset = h4s.read_partition( part_index )                     # Each partition corresponds to a single file, downloads file from s3 to local cache before reading.
 
@@ -24,8 +23,6 @@ for vid, v in ds0.attrs.items():
 print( f"\n ***  variables:")
 for vid, v in ds0.variables.items():
     print(f" ----> {vid}{v.dims} ({v.shape})")
-
-# stores = h4s.export( location=f"file:/{output_dir}" )    # Exports all partitions, Zarr is the default export format
 
 store = h4s.export( f"file:/{output_file}" )
 
