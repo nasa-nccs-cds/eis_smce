78d07e71efc64dbcba39c0d99acbf2ffbce0c28b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-05 11:47:56 -0400

    Update tests

--- eis_smce/data/intake/hdf4/tests/mfdataset.py
+++ eis_smce/data/intake/hdf4/tests/mfdataset.py
@@ -15,20 +15,20 @@ print( f"COORDS: {list(df0.coords.keys())}" )
 print( f"ITEMS: {list(df0.keys())}" )
 print( f"VARS: {list(df0.variables.keys())}" )
 
-exit(0)
-
-def preprocess_for_merge( ds: xr.Dataset ) -> xr.Dataset:
-    pass
+def preprocess( ds: xr.Dataset ):
+    new_vars = {}
+    for name, xar in ds.items():
+        new_vars[ name ] = xar.expand_dims( {"sample":np.array([ds.attrs['sample']])}, 0 )
+    return xr.Dataset( new_vars )
 
 for iF in file_indices:
     print( f"Processing data file: {data_files[iF]}" )
     single_dataset: xr.Dataset = xr.open_dataset( data_files[iF] )
-    unmerged_data_array: xr.DataArray = single_dataset[test_var].expand_dims( {"sample":np.array([iF])}, 0 )
-    print( f'\n\nunmerged_data_array[{iF}:' )
-    print( unmerged_data_array )
+    single_dataset.attrs['sample'] = str(iF)
+    single_dataset.to_netcdf( data_files[iF], "w" )
 
 merge_index = xr.IndexVariable( 'sample', np.array(file_indices) )
-merged_dataset: xr.Dataset = xr.open_mfdataset( data_files, concat_dim='sample' )   # preprocess to expand_dims:  preprocess=
+merged_dataset: xr.Dataset = xr.open_mfdataset( data_files, concat_dim='sample', preprocess=preprocess )
 merged_data_array: xr.DataArray = merged_dataset[test_var]
 print( '\n\nmerged_data_array:' )
 print( merged_data_array )
