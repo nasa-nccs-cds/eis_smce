bf01e3e044bd6686c3ecdea5981e6bdb81073093
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-05 18:06:55 -0400

    Update tests

--- eis_smce/data/intake/hdf4/tests/adapt-modis-css-MOD17A2H-GPP.py
+++ eis_smce/data/intake/hdf4/tests/adapt-modis-css-MOD17A2H-GPP.py
@@ -7,14 +7,13 @@ base_dir = "/css/modis/Collection6/L3/"
 cache_dir = "/att/nobackup/tpmaxwel/ILAB/scratch"
 collection = "MOD17A2H-GPP"
 year = "2004"
-day = "01?"
+day = "0*"
 file_names = "MOD17A2H.{sample}.h09v09.006.{sid}.hdf"
 batch = f"{year}/{day}/{file_names}"
 output_file = f"{cache_dir}/{collection}/h09v09.zarr"
 os.makedirs( os.path.dirname(output_file), exist_ok=True )
-part_index: int = 0
 
-data_url = f"file:/{base_dir}/{collection}/{batch}"
+data_url = f"file://{base_dir}/{collection}/{batch}"
 h4s: HDF4Source = HDF4Source( data_url  )                              # Creates source encapsulating all matched files in data_url
 h4s.export( output_file )
 zs = ZarrSource( output_file )
