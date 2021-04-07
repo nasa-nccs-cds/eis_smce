b4d7fe9543ce367190efb1ba1975f4592147d9c7
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-07 15:56:14 -0400

    Update file merge

--- eis_smce/data/intake/hdf4/tests/s3-fire-zarr.py
+++ eis_smce/data/intake/hdf4/tests/s3-fire-zarr.py
@@ -4,16 +4,16 @@ from typing import List, Union, Dict, Callable, Tuple, Optional, Any, Type, Mapp
 from intake_xarray.xzarr import ZarrSource
 
 part_index: int = 0
-data_url: str      = "s3://eis-dh-fire/mod14/raw/MOD14.{sample}.hdf"  # Matches files like glob: "s3://eis-dh-fire/mod14/raw/MOD14.*.hdf"
+data_url: str      = "s3://eis-dh-fire/mod14/raw/MOD14.{sample}.061.{tid}.hdf"  # Matches files like glob: "s3://eis-dh-fire/mod14/raw/MOD14.*.hdf"
 data_file_url: str = "s3://eis-dh-fire/mod14/raw/MOD14.A2020303.0515.061.2020349105630.hdf"
 
 if __name__ == '__main__':
-    h4s: HDF4Source = HDF4Source( data_file_url  )                        # Creates source encapsulating all matched files in data_url
+    h4s: HDF4Source = HDF4Source( data_url  )                             # Creates source encapsulating all matched files in data_url
     ds0: xr.Dataset = h4s.read_partition( part_index )                    # Each partition corresponds to a single file, downloads file from s3 to local cache before reading.
 
     remote_input_file: str = ds0.attrs['remote_file']
     local_input_file: str  = ds0.attrs['local_file']
-    remote_zarr_file: str = os.path.splitext( remote_input_file )[0] + ".zarr"
+    remote_zarr_file: str = os.path.expanduser( "~/.eis_smce/cache/mod14/raw/MOD14.061.hdf" )
 
     print(f"\n HDF4Source DATASET DS0:" )
     print( f"\n ***  attributes:")
