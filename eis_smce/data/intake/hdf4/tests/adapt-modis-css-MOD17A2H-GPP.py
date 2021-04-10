b3a61bfe6f8bf004da24297929feb4fb152e69df
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-04-09 20:55:38 -0400

    Update test

--- eis_smce/data/intake/hdf4/tests/adapt-modis-css-MOD17A2H-GPP.py
+++ eis_smce/data/intake/hdf4/tests/adapt-modis-css-MOD17A2H-GPP.py
@@ -16,7 +16,7 @@ data_url = f"file://{base_dir}/{collection}/{batch}"
 if __name__ == '__main__':
 
     h4s = intake.open_hdf4( data_url )
-    h4s.export( output_file )
+    h4s.export( output_file, cat_path=cat_path )
     zs = intake.open_zarr( output_file )
 
     print( "\nZarrSource:" )
