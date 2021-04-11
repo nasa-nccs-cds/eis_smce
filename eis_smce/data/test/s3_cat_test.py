56ee2f99993d443da746fac1c974b5c2e1f0544e
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-11 16:34:19 -0400

    Update s3

--- eis_smce/data/test/s3_cat_test.py
+++ eis_smce/data/test/s3_cat_test.py
@@ -1,10 +1,9 @@
 import intake
-from eis_smce.data.storage.s3 import s3m
 from eis_smce.data.intake.catalog import cm
 # intake.output_notebook()
 
 bucketname = 'eis-dh-fire'
 
 cm = cm( bucket = bucketname )
-cat = s3m().cat()
+cat = cm.cat()
 print( f" cat = {list(cat)}" )
