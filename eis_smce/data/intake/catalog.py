d3c67ef652d81bef2f3f44a8bb920018583cd0a7
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-11 17:02:16 -0400

    Update catalog

--- eis_smce/data/intake/catalog.py
+++ eis_smce/data/intake/catalog.py
@@ -14,9 +14,7 @@ class CatalogManager(tlc.SingletonConfigurable):
         self._s3 = None
         new_bucket = kwargs.get( 'bucket', None )
         if new_bucket: self.bucket = new_bucket
-        self.catalog_path: str = kwargs.get( 'cat_path', self.default_catalog_path )
-        print( f" Creating YAMLFilesCatalog with path = {self.catalog_path}, kwargs = {kwargs}")
-        self._cat: YAMLFilesCatalog = YAMLFilesCatalog( self.catalog_path )
+        self.file_path: str = kwargs.get( 'file_path', None )
 
     @property
     def s3(self):
@@ -24,24 +22,26 @@ class CatalogManager(tlc.SingletonConfigurable):
         return self._s3
 
     @property
-    def default_catalog_path(self) -> str:
+    def s3_cat_path(self) -> str:
         return f"s3://{self.bucket}/catalog"
 
+    @property
+    def cat_path(self) -> str:
+        return self.file_path or self.s3_cat_path
+
     def cat( self ) -> intake.Catalog:
-        cat_path = f"{self.catalog_path}/*.yml"
+        cat_path = f"{self.cat_path}/*.yml"
         return intake.open_catalog( cat_path )
 
     def addEntry( self, source: EISZarrSource, **kwargs ):
         entry_yml = source.yaml( **kwargs )
-        catalog = f"{self.catalog_path}/{source.cat_name}.yml"
-        print( f"addEntry: Catalog={catalog}, Entry = {entry_yml}" )
-        if catalog.startswith("s3:"):  self.s3.Object( self.bucket, catalog ).put( Body=entry_yml )
-        else:                          self.write_cat_file( catalog, entry_yml )
-        self._cat.reload()
-
-    @property
-    def cat(self) -> YAMLFilesCatalog:
-        return self._cat
+        if self.file_path is None:
+            catalog = f"catalog/{source.cat_name}.yml"
+            self.s3.Object( self.bucket, catalog ).put( Body=entry_yml )
+        else:
+            catalog = f"{self.file_path}/{source.cat_name}.yml"
+            self.write_cat_file( catalog, entry_yml )
+        print(f"addEntry: Catalog={catalog}, Entry = {entry_yml}")
 
     def write_cat_file(self, catalog: str, entry: str ):
         with open( catalog, "w" ) as fp:
