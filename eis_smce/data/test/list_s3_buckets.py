f411fd45074ba81ec2113da47571aaeca899a443
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-11 18:26:15 -0400

    Update catalog

--- eis_smce/data/test/list_s3_buckets.py
+++ eis_smce/data/test/list_s3_buckets.py
@@ -1,9 +1,12 @@
 import boto3
 
+bucketname = 'eis-dh-fire'
+
 s3r = boto3.resource('s3')
 for bucket in s3r.buckets.all():
      print( bucket.name )
 
-
-print( s3r.__class__ )
+print( "Catalog objects:")
+bucket = s3r.Bucket(bucketname)
+for obj in bucket.objects.filter(Prefix="catalog"):
 
