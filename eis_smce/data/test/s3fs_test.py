59ce053ea62595a013f71748429ef6d68f7512de
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-11 17:42:08 -0400

    Update catalog

--- eis_smce/data/test/s3fs_test.py
+++ eis_smce/data/test/s3fs_test.py
@@ -1,12 +1,16 @@
 import s3fs
 import os
+#aws_key_id = os.environ.get( 'AWS_ACCESS_KEY_ID' )
+#aws_secret_key = os.environ.get( 'AWS_ACCESS_KEY_ID' )
+#region="us-east-1"
 
 bucketname = 'eis-dh-fire'
-aws_key_id = os.environ.get( 'AWS_ACCESS_KEY_ID' )
-aws_secret_key = os.environ.get( 'AWS_ACCESS_KEY_ID' )
-region="us-east-1"
-
+source = "MOD13Q1-Vegetation"
 s3f: s3fs.S3FileSystem  = s3fs.S3FileSystem()
+
+print( "Data Files:")
+print( s3f.ls( f"/{bucketname}/{source}" ) )
+
 print( "Catalog Files:")
-print( s3f.ls( f"/{bucketname}/MOD13Q1-Vegetation" ) )
+print( s3f.ls( f"/{bucketname}/catalog" ) )
 
