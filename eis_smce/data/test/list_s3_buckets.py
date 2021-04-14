2810000a2d2c11e341541258ffc474d0cb1707ca
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-14 16:10:23 -0400

    Add freshwater workflow

--- eis_smce/data/test/list_s3_buckets.py
+++ eis_smce/data/test/list_s3_buckets.py
@@ -1,14 +1,13 @@
 import boto3
 from eis_smce.data.storage.s3 import s3m
 
-bucketname = 'eis-dh-fire'
+bucketname = 'eis-dh-hydro'
 # prefix = "catalog/MOD13Q1"
-prefix = 's3://eis-dh-hydro/projects/eis_freshwater/'
+prefix = 'projects/eis_freshwater/'
 s3r = boto3.resource('s3')
 
 print( "Catalog objects:")
 bucket = s3r.Bucket(bucketname)
 for obj in bucket.objects.filter( Prefix=prefix ):
      print( f"s3://{obj.bucket_name}/{obj.key}" )
-     s3m().set_acl(obj.key)
 
