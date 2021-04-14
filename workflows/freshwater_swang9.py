22dc0092024a8fd2bf5bb6af2aa65735e875bd43
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-14 16:19:01 -0400

    Add freshwater workflow

--- workflows/freshwater_swang9.py
+++ workflows/freshwater_swang9.py
@@ -5,7 +5,7 @@ input_dir = "/discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST
 cache_dir = "/discover/nobackup/tpmaxwel/cache"
 name = "freshwater.swang9.OL_1km.2013"
 bucket = "eis-dh-hydro"
-month = "201302"
+month = "*"
 s3_prefix = f"projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013"
 
 dsets = [
