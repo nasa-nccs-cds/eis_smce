8f4866364cb1df39905ed5012c2c7bcb01489bab
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-15 18:40:04 -0400

    Add freshwater workflow

--- eis_smce/data/intake/source.py
+++ eis_smce/data/intake/source.py
@@ -102,7 +102,7 @@ class EISDataSource( DataSource ):
         self._load_metadata()
         self.merge_dim = kwargs.get('merge_dim', self.merge_dim)
         file_list = self.get_file_list()
-        parallel = kwargs.get( 'parallel_merge', True )
+        parallel = kwargs.get( 'parallel_merge', False )
         t0 = time.time()
         print( f"Reading merged dataset from {len(file_list)} files, parallel = {parallel}" )
         rv = xa.open_mfdataset( file_list, concat_dim=self.merge_dim, coords="minimal", data_vars="all", parallel=parallel )
