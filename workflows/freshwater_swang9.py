8436a83a6cc4d66a83fcb4cf370e0fc949766f4b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-15 18:28:57 -0400

    Add freshwater workflow

--- workflows/freshwater_swang9.py
+++ workflows/freshwater_swang9.py
@@ -7,7 +7,6 @@ cache_dir = "/discover/nobackup/tpmaxwel/cache"
 name = "freshwater.swang9.OL_1km.2013"
 bucket = "eis-dh-hydro"
 month = "201302" if reduced_run else  "*"
-nparallel = 1
 s3_prefix = f"projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013"
 
 dsets = [
@@ -29,7 +28,7 @@ if __name__ == '__main__':
     for dset in dsets:
         [input, output] = [dset.pop(key) for key in [ 'input', 'output' ] ]
         h4s = intake.open_hdf4( input, cache_dir=cache_dir )
-        zs = h4s.export( output, bucket=bucket, merge_dim = "time", nparallel=nparallel, **dset )
+        zs = h4s.export( output, bucket=bucket, merge_dim = "time", **dset )
         cm().addEntry( zs  )
 
     cm().write_s3( bucket, name )
