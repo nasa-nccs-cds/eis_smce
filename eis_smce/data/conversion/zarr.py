a1503b99c7f2b3ac52fa68cd361e12a4b797abe3
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-15 13:08:41 -0400

    Add freshwater workflow

---
+++ eis_smce/data/conversion/zarr.py
@@ -0,0 +1,15 @@
+import os, traitlets.config as tlc
+import zarr
+from eis_smce.data.intake.hdf4.drivers import HDF4Source
+from typing import List, Union, Dict, Callable, Tuple, Optional, Any, Type, Mapping, Hashable
+
+class ZarrConverter(tlc.SingletonConfigurable):
+
+    default_cache_dir = tlc.Unicode( os.path.expanduser("~/.eis_smce/cache") ).tag(config=True)
+
+    def __init__( self, **kwargs ):
+        tlc.SingletonConfigurable.__init__( self, **kwargs )
+        self.cache_dir = kwargs.get('cache', self.default_cache_dir )
+        os.makedirs( self.cache_dir, exist_ok=True )
+
+    def standard_conversion(self, destination: str):
