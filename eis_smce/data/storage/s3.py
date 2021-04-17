7b18481a3ccd166ff9233d337930031daf2e87d8
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-17 13:01:59 -0400

    Add logging

--- eis_smce/data/storage/s3.py
+++ eis_smce/data/storage/s3.py
@@ -59,7 +59,8 @@ class S3Manager(EISSingleton):
         return file_path
 
     def get_store(self, path: str ) -> MutableMapping:
-        self.fs.delete( path, recursive=True )
+        try: self.fs.delete( path, recursive=True )
+        except FileNotFoundError: pass
         return s3fs.S3Map( root=path, s3=self.fs, check=False, create=True )
 
     def upload_files(self, src_path: str, dest_path: str ):
