0f756efc46e4a186f7f01448802c390cf8f252a2
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-18 12:07:20 -0400

    Add logging

--- workflows/freshwater_swang9.py
+++ workflows/freshwater_swang9.py
@@ -11,7 +11,7 @@ input_dir = "/discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST
 name = "freshwater.swang9.OL_1km.2013"
 bucket = "eis-dh-hydro"
 month = "201303" if test_run else  "*"
-s3_prefix = f"projects/eis_freshwater/swang9.OL_1km.2013.0"
+s3_prefix = f"projects/eis_freshwater/swang9.OL_1km.2013.3"
 eisc( cache = "/discover/nobackup/tpmaxwel/cache", mode = "freshwater.swang9" )
 
 dsets = [
