611f4793da0081d4967ed85cb83f35b7e0b82a49
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-18 17:22:48 -0400

    Add logging

--- workflows/freshwater_swang9.py
+++ workflows/freshwater_swang9.py
@@ -7,20 +7,20 @@ cache_dir = "/discover/nobackup/tpmaxwel/cache"
 name = "freshwater.swang9.OL_1km.2013"
 bucket = "eis-dh-hydro"
 month = "201302" if reduced_run else  "*"
-s3_prefix = f"projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013"
+s3_prefix = f"projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013.001"
 
 dsets = [
     # dict(   input = f"file://{input_dir}/ROUTING/{month}/LIS_RST_HYMAP2_router" + "_{time}.d01.nc",
     #         output = f"s3://{bucket}/{s3_prefix}/ROUTING/LIS_RST_HYMAP2_router.d01.zarr" ),
 
-    dict(   input=f"file://{input_dir}/ROUTING/{month}/LIS_HIST" + "_{time}.d01.nc",
-            output=f"s3://{bucket}/{s3_prefix}/ROUTING/LIS_HIST.d01.zarr"  ),
+    # dict(   input=f"file://{input_dir}/ROUTING/{month}/LIS_HIST" + "_{time}.d01.nc",
+    #         output=f"s3://{bucket}/{s3_prefix}/ROUTING/LIS_HIST.d01.zarr"  ),
 
     # dict(   input=f"file://{input_dir}/SURFACEMODEL/{month}/LIS_HIST" + "_{time}.d01.nc",
     #         output=f"s3://{bucket}/{s3_prefix}/SURFACEMODEL/LIS_HIST.d01.zarr" ),
     #
-    # dict(   input=f"file://{input_dir}/SURFACEMODEL/{month}/LIS_RST_NOAHMP401" + "_{time}.d01.nc",
-    #         output=f"s3://{bucket}/{s3_prefix}/SURFACEMODEL/LIS_RST_NOAHMP401.d01.zarr"  ),
+    dict(   input=f"file://{input_dir}/SURFACEMODEL/{month}/LIS_RST_NOAHMP401" + "_{time}.d01.nc",
+            output=f"s3://{bucket}/{s3_prefix}/SURFACEMODEL/LIS_RST_NOAHMP401.d01.zarr"  ),
 ]
 
 if __name__ == '__main__':
