27fab7ce981b97d66878f93bba6806502592bed2
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-20 22:51:22 -0400

    Refactor export

--- workflows/freshwater_swang9.py
+++ workflows/freshwater_swang9.py
@@ -12,7 +12,7 @@ name = "freshwater.swang9.OL_1km.2013"
 bucket = "eis-dh-hydro"
 month = "201303" if test_run else  "*"
 s3_prefix = f"projects/eis_freshwater/swang9.OL_1km.2013.new"
-eisc( cache = "/discover/nobackup/tpmaxwel/cache", mode = "freshwater.swang9" )
+eisc( cache = "/discover/nobackup/tpmaxwel/cache", mode = "eis.freshwater.swang9" )
 
 dsets = [
     # dict(   input = f"file://{input_dir}/ROUTING/{month}/LIS_RST_HYMAP2_router" + "_{time}.d01.nc",
@@ -22,7 +22,8 @@ dsets = [
     #        output=f"s3://{bucket}/{s3_prefix}/ROUTING/LIS_HIST.d01.zarr"  ),
 
     dict(   input=f"file://{input_dir}/SURFACEMODEL/{month}/LIS_HIST" + "_{time}.d01.nc",
-            output=f"s3://{bucket}/{s3_prefix}/SURFACEMODEL/LIS_HIST.d01.zarr" ),
+#            output=f"s3://{bucket}/{s3_prefix}/SURFACEMODEL/LIS_HIST.d01.zarr" ),
+            output=f"/discover/nobackup/tpmaxwel/cache/eis.freshwater.swang9/output/SURFACEMODEL/LIS_HIST.d01.zarr" ),
 
  #   dict(   input=f"file://{input_dir}/SURFACEMODEL/{month}/LIS_RST_NOAHMP401" + "_{time}.d01.nc",
  #           output=f"s3://{bucket}/{s3_prefix}/SURFACEMODEL/LIS_RST_NOAHMP401.d01.zarr"  ),
