9d2b051d8bcc1c878354db55b6c2a5c2afb893a1
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-22 17:18:56 -0400

    Refactor export

--- workflows/freshwater_swang9.py
+++ workflows/freshwater_swang9.py
@@ -15,14 +15,14 @@ s3_prefix = f"projects/eis_freshwater/swang9.OL_1km.2013.new"
 eisc( cache = "/gpfsm/dnb43/projects/p151/zarr", mode = "eis.freshwater.swang9" )
 
 dsets = [
-    #dict(   input = f"file://{input_dir}/ROUTING/{month}/LIS_RST_HYMAP2_router" + "_{time}.d01.nc",
-    #        output=f"/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/LIS_RST_HYMAP2_router.d01.zarr"  ),
+    dict(   input = f"file://{input_dir}/ROUTING/{month}/LIS_RST_HYMAP2_router" + "_{time}.d01.nc",
+            output=f"/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/LIS_RST_HYMAP2_router.d01.zarr"  ),
 
 #    dict(   input=f"file://{input_dir}/ROUTING/{month}/LIS_HIST" + "_{time}.d01.nc",
  #           output=f"s3://{bucket}/{s3_prefix}/ROUTING/LIS_HIST.d01.zarr"  ),
 
-    dict(   input=f"file://{input_dir}/SURFACEMODEL/{month}/LIS_HIST" + "_{time}.d01.nc",
-            output=f"/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/SURFACEMODEL/LIS_HIST.d01.zarr" ),
+#    dict(   input=f"file://{input_dir}/SURFACEMODEL/{month}/LIS_HIST" + "_{time}.d01.nc",
+#            output=f"/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/SURFACEMODEL/LIS_HIST.d01.zarr" ),
 #
 #    dict(   input=f"file://{input_dir}/SURFACEMODEL/{month}/LIS_RST_NOAHMP401" + "_{time}.d01.nc",
 #            output=f"/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/SURFACEMODEL/LIS_RST_NOAHMP401.d01.zarr"  ),
