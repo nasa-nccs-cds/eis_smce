52d60cfcb33f84e3d9ae1a6567c5c8eea0f96c87
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-23 15:06:20 -0400

    Refactor export

--- workflows/zarr_file_test_read.py
+++ workflows/zarr_file_test_read.py
@@ -3,7 +3,7 @@ import numpy as np
 
 sample_input = "/discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013/SURFACEMODEL/201402/LIS_HIST_201402080000.d01.nc"
 zarr_dest = "/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/LIS_RST_HYMAP2_router.d01.zarr"
-vname = "FLDSTO"
+vname = "BSFSTO"
 
 ids: xa.Dataset = xa.open_dataset( sample_input )
 zds: xa.Dataset = xa.open_zarr( zarr_dest )
@@ -19,11 +19,11 @@ for k,v in zds.attrs.items():
 print( f"\nzds:"  )
 print( zds )
 
-RNFSTO: xa.DataArray = zds[vname]
-print( f"\n{vname} shape: {RNFSTO.shape}"  )
+test_array: xa.DataArray = zds[vname]
+print( f"\n{vname} shape: {test_array.shape}"  )
 
-print( f"{vname} attrs: {RNFSTO.attrs}"  )
+print( f"{vname} attrs: {test_array.attrs}"  )
 
-test_data: np.ndarray = RNFSTO.max( axis = 1 ).values
+test_data: np.ndarray = test_array.max( axis = 1 ).values
 print( f"{vname} max (axis=1):"  )
 print( test_data )
