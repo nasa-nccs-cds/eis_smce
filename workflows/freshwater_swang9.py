0d91758397eb25aa14bc628891d2e90c5d398be1
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-23 15:20:12 -0400

    Refactor export

--- workflows/freshwater_swang9.py
+++ workflows/freshwater_swang9.py
@@ -16,13 +16,13 @@ eisc( cache = "/gpfsm/dnb43/projects/p151/zarr", mode = "eis.freshwater.swang9" 
 time_format = "%Y%m%d%H%M"
 
 dsets = [
-    dict(   input = f"file://{input_dir}/ROUTING/{month}/LIS_RST_HYMAP2_router" + "_{time}.d01.nc",
-            output=f"/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/LIS_RST_HYMAP2_router.d01.zarr",
-            time_format = time_format ),
-
-#    dict(   input=f"file://{input_dir}/ROUTING/{month}/LIS_HIST" + "_{time}.d01.nc",
- #           output=f"s3://{bucket}/{s3_prefix}/ROUTING/LIS_HIST.d01.zarr",
+#    dict(   input = f"file://{input_dir}/ROUTING/{month}/LIS_RST_HYMAP2_router" + "_{time}.d01.nc",
+#            output=f"/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/LIS_RST_HYMAP2_router.d01.zarr",
 #            time_format = time_format ),
+
+    dict(   input=f"file://{input_dir}/ROUTING/{month}/LIS_HIST" + "_{time}.d01.nc",
+            output=f"s3://{bucket}/{s3_prefix}/ROUTING/LIS_HIST.d01.zarr",
+            time_format = time_format ),
 
 #    dict(   input=f"file://{input_dir}/SURFACEMODEL/{month}/LIS_HIST" + "_{time}.d01.nc",
 #            output=f"/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/SURFACEMODEL/LIS_HIST.d01.zarr",
--- workflows/zarr_file_test_read.py
+++ workflows/zarr_file_test_read.py
@@ -1,8 +1,8 @@
 import xarray as xa
 import numpy as np
 
-sample_input = "/discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013/SURFACEMODEL/201402/LIS_HIST_201402080000.d01.nc"
-zarr_dest = "/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/LIS_RST_HYMAP2_router.d01.zarr"
+sample_input = "/discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013/ROUTING/201501/LIS_RST_HYMAP2_router_201501312345.d01.nc"
+zarr_dest = "/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/201402/LIS_RST_HYMAP2_router.d01.zarr"
 vname = "FLDDPH_PRE"
 
 ids: xa.Dataset = xa.open_dataset( sample_input )
@@ -32,6 +32,6 @@ input_array: xa.DataArray = ids[vname]
 print( f"\n{vname} input_array shape: {input_array.shape}"  )
 print( f"{vname} input_array attrs: {input_array.attrs}"  )
 input_data: np.ndarray = input_array.max( ).values
-print( f"{vname} max (axis=1):"  )
+print( f"{vname} max:"  )
 print( input_data )
 
