3e76b4a617bd5ae672f86d92e5aeea293a5ac389
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-23 14:34:09 -0400

    Refactor export

--- workflows/zarr_file_test_read.py
+++ workflows/zarr_file_test_read.py
@@ -1,20 +1,28 @@
 import xarray as xa
 import numpy as np
 
+sample_input = "/discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013/SURFACEMODEL/201402/LIS_HIST_201402080000.d01.nc"
 zarr_dest = "/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/LIS_RST_HYMAP2_router.d01.zarr"
 
-dset: xa.Dataset = xa.open_zarr( zarr_dest )
+ids: xa.Dataset = xa.open_dataset( sample_input )
+zds: xa.Dataset = xa.open_zarr( zarr_dest )
 
-print( dset )
+print( f"\nids attrs: {ids.attrs}"  )
+for k,v in ids.attrs.items():
+    print( f"  **  {k}: {v}")
 
-print( f"dset attrs: {dset.attrs}"  )
+print( f"\nzds attrs: {zds.attrs}"  )
+for k,v in zds.attrs.items():
+    print( f"  **  {k}: {v}")
 
-RNFSTO: xa.DataArray = dset['RNFSTO']
+print( f"\nzds:"  )
+print( zds )
 
-print( f"RNFSTO shape: {RNFSTO.shape}"  )
+RNFSTO: xa.DataArray = zds['RNFSTO']
+print( f"\nRNFSTO shape: {RNFSTO.shape}"  )
 
 print( f"RNFSTO attrs: {RNFSTO.attrs}"  )
 
-test_data: np.ndarray = RNFSTO.max( axis = 1 )
+test_data: np.ndarray = RNFSTO.max( axis = 1 ).values
 print( f"RNFSTO max (axis=1):"  )
 print( test_data )
