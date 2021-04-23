e430331bf6a77d472af50c9c120daafc464e5aa8
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-23 14:18:54 -0400

    Refactor export

--- workflows/zarr_file_test_read.py
+++ workflows/zarr_file_test_read.py
@@ -1,7 +1,14 @@
 import xarray as xa
+import numpy as np
 
 zarr_dest = "/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/LIS_RST_HYMAP2_router.d01.zarr"
 
 dset: xa.Dataset = xa.open_zarr( zarr_dest )
 
 print( dset )
+
+RNFSTO: xa.DataArray = dset['RNFSTO']
+
+test_data: np.ndarray = RNFSTO[ :, 2509494 ].values
+
+print( test_data )
