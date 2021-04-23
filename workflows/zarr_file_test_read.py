3819125f190aad97882e5efeb19d6b3a65863de6
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-23 15:08:35 -0400

    Refactor export

--- workflows/zarr_file_test_read.py
+++ workflows/zarr_file_test_read.py
@@ -3,7 +3,7 @@ import numpy as np
 
 sample_input = "/discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013/SURFACEMODEL/201402/LIS_HIST_201402080000.d01.nc"
 zarr_dest = "/gpfsm/dnb43/projects/p151/zarr/freshwater.swang.2013/output/ROUTING/LIS_RST_HYMAP2_router.d01.zarr"
-vname = "BSFSTO"
+vname = "FLDDPH_PRE"
 
 ids: xa.Dataset = xa.open_dataset( sample_input )
 zds: xa.Dataset = xa.open_zarr( zarr_dest )
