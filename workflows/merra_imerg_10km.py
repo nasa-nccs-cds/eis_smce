164e70aa2d096038bf239073986f8ac2d54b773f
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-04-29 15:28:34 -0400

    Update docs

--- workflows/merra_imerg_10km.py
+++ workflows/merra_imerg_10km.py
@@ -4,7 +4,7 @@ from eis_smce.data.common.base import eisc
 from eis_smce.data.conversion.zarr import zc
 from eis_smce.data.common.cluster import dcm
 
-test_run = True
+test_run = False
 input_dir = "/discover/nobackup/projects/eis_freshwater/swang9/OL_10km/OUTPUT.1980.imerg.fixed"
 bucket = "eis-dh-hydro"
 month = "199312" if test_run else  "*"
