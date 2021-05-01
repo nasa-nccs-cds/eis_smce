0c4b5e556e9762f8d023b08fbece0d967d08cecc
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-05-01 15:39:14 -0400

    Add file sort

--- eis_smce/data/storage/local.py
+++ eis_smce/data/storage/local.py
@@ -15,42 +15,49 @@ class FileSortKey(Enum):
     pattern = 2
     coordinate = 3
 
-    def key(self, collection_specs: Dict = None ):
-        if self.value == self.filename: return partial(self.filename_key,collection_specs)
-        if self.value == self.pattern: return partial(self.pattern_key,collection_specs)
-        if self.value == self.coordinate: return partial(self.coordinate_key,collection_specs)
+    def key(self, collection_specs: Dict, file_specs: Dict ):
+        return self.sort_key_method( collection_specs, file_specs )
+
+    @property
+    def sort_key_method( self ):
+        if self.value == self.filename:   return self.filename_key
+        if self.value == self.pattern:    return self.pattern_key
+        if self.value == self.coordinate: return self.coordinate_key
 
     @staticmethod
-    def filename_key( collection_specs: Dict, item: Dict ):
-        return  os.path.basename( item['resolved'] )
+    def filename_key( collection_specs: Dict, file_specs: Dict ):
+        return os.path.basename( file_specs['resolved'] )
 
     @staticmethod
-    def pattern_key( collection_specs: Dict, item: Dict ):
+    def pattern_key( collection_specs: Dict, file_specs: Dict ):
         merge_dim = collection_specs.get('merge_dim','time')
         time_format = collection_specs.get('time_format', None )
-        return  item[merge_dim] if time_format is None else datetime.strptime( item[merge_dim], time_format)
+        return file_specs[merge_dim] if time_format is None else datetime.strptime( file_specs[merge_dim], time_format)
 
     @staticmethod
-    def coordinate_key( collection_specs: Dict, item: Dict ):
-        with xa.open_dataset( item['resolved'] ) as dset:
+    def coordinate_key( collection_specs: Dict, file_specs: Dict ):
+        with xa.open_dataset( file_specs['resolved'] ) as dset:
             merge_dim = collection_specs.get('merge_dim', 'time')
-            return dset[merge_dim].values[0]
-
+            return  dset[merge_dim].values[0]
 
 class LocalFileManager(EISSingleton ):
 
     def __init__( self, **kwargs ):
         EISSingleton.__init__( self, **kwargs )
-        self._file_sort = FileSortKey[ kwargs.get( 'sort', 'name' ) ]
 
     def _parse_urlpath( self, urlpath: str ) -> str:
         return urlpath.split(":")[-1].replace("//","/").replace("//","/")
+
+    @staticmethod
+    def sort_key( item: Dict ):
+        return item['sort_key']
 
     def get_file_list(self, urlpath: str, collection_specs: Dict ) -> List[Dict]:
         from intake.source.utils import reverse_format
         filepath_pattern = self._parse_urlpath( urlpath )
         filepath_glob = path_to_glob( filepath_pattern )
         input_files = glob.glob(filepath_glob)
+        file_sort = FileSortKey[ collection_specs.get('sort', 'name') ]
         is_glob = has_char( filepath_pattern, "*?[" )
         files_list = []
         self.logger.info(f" Processing {len(input_files)} input files from glob '{filepath_glob}'")
@@ -59,8 +66,9 @@ class LocalFileManager(EISSingleton ):
                 (file_name, file_pattern) = (os.path.basename(file_path) , os.path.basename(filepath_pattern)) if is_glob else (file_path,filepath_pattern)
                 metadata = reverse_format( file_pattern, file_name )
                 metadata['resolved'] = file_path
+                metadata['sort_key'] = file_sort.key( collection_specs, metadata )
                 files_list.append(metadata)
             except ValueError as err:
                 self.logger.error( f" Metadata processing error: {err}, Did you mix glob and pattern in file name?")
-        files_list.sort( key=self._file_sort.key(collection_specs) )
+        files_list.sort( key=self.sort_key )
         return files_list
