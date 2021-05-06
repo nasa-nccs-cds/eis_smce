ec596256c64a9e69833405443f76dafe714cc66b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-05-06 17:56:59 -0400

    Refactor to add database segments

---
+++ workflows/simple_zarr_conversion.py
@@ -0,0 +1,10 @@
+import glob, xarray as xa
+
+input_path = "/discover/nobackup/projects/eis_freshwater/lahmers/RUN/1km_DOMAIN_DAens20_MCD15A2H.006_2019Flood/OUTPUT/SURFACEMODEL/*/LIS_HIST*.nc"
+input_files = glob.glob( input_path )
+merge_dim = "time"
+cache_dir = "/discover/nobackup/tpmaxwel/cache"
+zarr_file = f"{cache_dir}/zarr_test.zarr"
+
+dset: xa.Dataset = xa.open_mfdataset( input_files, concat_dim=merge_dim, coords="minimal", parallel = True )
+dset.to_zarr( zarr_file, mode="w" )
