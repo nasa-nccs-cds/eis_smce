ee46646cb009792dcd20251a896031e96f2eb17b
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-05-07 12:53:10 -0400

    Refactor to add database segments

--- workflows/simple_zarr_conversion.py
+++ workflows/simple_zarr_conversion.py
@@ -1,11 +1,12 @@
 import glob, time, xarray as xa
 
 input_path0 = "/discover/nobackup/projects/eis_freshwater/lahmers/RUN/1km_DOMAIN_DAens20_MCD15A2H.006_2019Flood/OUTPUT/SURFACEMODEL/*/LIS_HIST*.nc"
-input_path = "/discover/nobackup/projects/eis_freshwater/swang9/OL_10km/OUTPUT.1980.imerg.fixed/SURFACEMODEL/*/LIS_HIST*.nc"
+input_path1 = "/discover/nobackup/projects/eis_freshwater/swang9/OL_10km/OUTPUT.1980.imerg.fixed/SURFACEMODEL/*/LIS_HIST*.nc"
+input_path = "/discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013/ROUTING/*/LIS_HIST*.nc"    # 170 GB
 input_files = glob.glob( input_path )
 merge_dim = "time"
 cache_dir = "/discover/nobackup/tpmaxwel/cache"
-zarr_file = f"{cache_dir}/imerg.fixed.test.zarr"
+zarr_file = f"{cache_dir}/test.1km.routing.zarr"
 
 t0 = time.time()
 print( f"Opening dataset glob: {input_path}")
