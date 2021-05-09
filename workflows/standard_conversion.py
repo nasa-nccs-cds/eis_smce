886cd9b9cf3538d72e0d0c5331e7955078605a5e
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-05-08 23:51:34 -0400

    Refactor to add database segments

--- workflows/standard_conversion.py
+++ workflows/standard_conversion.py
@@ -9,18 +9,21 @@ logger.setLevel(logging.ERROR)
 eisc( cache="/gpfsm/dnb43/projects/p151/zarr", mode="eis.freshwater.swang9", time_format="%Y%m%d%H%M", batch_size=1000, merge_dim='time' )
 
 input_dir = "/discover/nobackup/projects/eis_freshwater"
+output_dir = "/gpfsm/dnb43/projects/p151/zarr"
 imerg_fixed_10km = dict(  input=f"file://{input_dir}/swang9/OL_10km/OUTPUT.1980.imerg.fixed/SURFACEMODEL/*/LIS_HIST" + "_{time}.d01.nc",
-                        output=f"/gpfsm/dnb43/projects/p151/zarr/LIS/OL_10km/1980/MERRA_IMERG"  )
-routing_2013_1km = dict(  input=f"file://discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2013/ROUTING/**/LIS_HIST*.nc",
-                          output=f"/gpfsm/dnb43/projects/p151/zarr/LIS/OL_1km/ROUTING/LIS_HIST.d01"  )
-merra_2000_1km = dict(  input=f"file://discover/nobackup/projects/eis_freshwater/swang9/OL_1km/OUTPUT.RST.2000/SURFACEMODEL/**/LIS_HIST*.nc",
-                        output=f"LIS/OL_1km/2000_2021/MERRA/LIS_HIST.d01"  )
+                        output=f"{output_dir}/LIS/OL_10km/1980/MERRA_IMERG"  )
+routing_2013_1km = dict(  input=f"file:/{input_dir}/swang9/OL_1km/OUTPUT.RST.2013/ROUTING/**/LIS_HIST*.nc",
+                          output=f"{output_dir}/LIS/OL_1km/ROUTING/LIS_HIST.d01"  )
+merra_2000_1km = dict(  input=f"file:/{input_dir}/swang9/OL_1km/OUTPUT.RST.2000/SURFACEMODEL/**/LIS_HIST*.nc",
+                        output=f"{output_dir}/LIS/OL_1km/2000_2021/MERRA/LIS_HIST.d01"  )
+merra_imerg_2000_1km = dict(  input=f"file:/{input_dir}/swang9/OL_1km/OUTPUT.RST.2000.imerg.fixed/SURFACEMODEL/*/LIS_HIST*.nc",
+                              output=f"{output_dir}/LIS/OL_1km/2000_2021/MERRA_IMERG/LIS_HIST.d01" )
 
 if __name__ == '__main__':
 
     dcm().init_cluster( processes=True )
 
-    dset = merra_2000_1km
+    dset = merra_imerg_2000_1km
     zc().standard_conversion( dset['input'],  dset['output'] )
 
     dcm().shutdown()
