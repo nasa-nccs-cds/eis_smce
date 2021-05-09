50e29d35a28d3f1c8cc5b165108dd09237d27a09
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-05-09 18:12:03 -0400

    Refactor to add database segments

--- workflows/standard_conversion.py
+++ workflows/standard_conversion.py
@@ -6,17 +6,19 @@ from eis_smce.data.common.cluster import dcm
 logger = logging.getLogger("distributed.utils_perf")
 logger.setLevel(logging.ERROR)
 
+partial_run = True
 eisc( cache="/gpfsm/dnb43/projects/p151/zarr", mode="eis.freshwater.swang9", time_format="%Y%m%d%H%M", batch_size=1000, merge_dim='time' )
 
+month = "200304" if partial_run else "**"
 input_dir = "/discover/nobackup/projects/eis_freshwater"
 output_dir = "/gpfsm/dnb43/projects/p151/zarr"
-imerg_fixed_10km = dict(  input=f"file://{input_dir}/swang9/OL_10km/OUTPUT.1980.imerg.fixed/SURFACEMODEL/*/LIS_HIST" + "_{time}.d01.nc",
+imerg_fixed_10km = dict(  input=f"file://{input_dir}/swang9/OL_10km/OUTPUT.1980.imerg.fixed/SURFACEMODEL/{month}/LIS_HIST" + "_{time}.d01.nc",
                         output=f"{output_dir}/LIS/OL_10km/1980/MERRA_IMERG"  )
-routing_2013_1km = dict(  input=f"file:/{input_dir}/swang9/OL_1km/OUTPUT.RST.2013/ROUTING/**/LIS_HIST*.nc",
+routing_2013_1km = dict(  input=f"file:/{input_dir}/swang9/OL_1km/OUTPUT.RST.2013/ROUTING/{month}/LIS_HIST*.nc",
                           output=f"{output_dir}/LIS/OL_1km/ROUTING/LIS_HIST.d01"  )
-merra_2000_1km = dict(  input=f"file:/{input_dir}/swang9/OL_1km/OUTPUT.RST.2000/SURFACEMODEL/**/LIS_HIST*.nc",
+merra_2000_1km = dict(  input=f"file:/{input_dir}/swang9/OL_1km/OUTPUT.RST.2000/SURFACEMODEL/{month}/LIS_HIST*.nc",
                         output=f"{output_dir}/LIS/OL_1km/2000_2021/MERRA/LIS_HIST.d01"  )
-merra_imerg_2000_1km = dict(  input=f"file:/{input_dir}/swang9/OL_1km/OUTPUT.RST.2000.imerg.fixed/SURFACEMODEL/*/LIS_HIST*.nc",
+merra_imerg_2000_1km = dict(  input=f"file:/{input_dir}/swang9/OL_1km/OUTPUT.RST.2000.imerg.fixed/SURFACEMODEL/{month}/LIS_HIST*.nc",
                               output=f"{output_dir}/LIS/OL_1km/2000_2021/MERRA_IMERG/LIS_HIST.d01" )
 
 if __name__ == '__main__':
