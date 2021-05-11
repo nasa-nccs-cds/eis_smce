afd10c8db920d39b923249a9209c98a2ac180754
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-05-11 15:30:45 -0400

    Refactor to add database segments

--- workflows/standard_conversion.py
+++ workflows/standard_conversion.py
@@ -27,7 +27,7 @@ MCD15A2H_2019FloodSM   = dict(  input=f"lahmers/RUN/1km_DOMAIN_DAens20_MCD15A2H.
                                 output=f"LIS/DA_1km/MODIS_Flood_2019/SURFACEMODEL/LIS_HIST.d01" )
 MCD15A2H_2019Drought   = dict(  input=f"/discover/nobackup/projects/eis_freshwater/lahmers/RUN/1km_DOMAIN_DAens20_MCD15A2H.006_2019Drought/OUTPUT/SURFACEMODEL/**/LIS_HIST*.nc",
                                 output=f"LIS/DA_1km/MODIS_Drought_2019/SURFACEMODEL/LIS_HIST.d01" )
-MCD15A2H_2002Drought   = dict(  input=f"/discover/nobackup/projects/eis_freshwater/lahmers/RUN/10km_DOMAIN_DAens20_MCD15A2H.006_2002_2021/OUTPUT.DA/SURFACEMODEL/**/LIS_HIST*.nc",    # Needs fixing.
+MCD15A2H_2002Drought   = dict(  input=f"ahmers/RUN/10km_DOMAIN_DAens20_MCD15A2H.006_2002_2021/OUTPUT.DA/SURFACEMODEL/**/LIS_HIST*.nc",    # Needs fixing.
                                 output=f"LIS/DA_10km/MODIS_Droughts_2002_2021/SURFACEMODEL/LIS_HIST.d01" )
 AMSR2                  = dict(  input=f"mwrzesie/Runs_10km/DA_AMSR2/OUTPUT_DA_AMSR2/SURFACEMODEL/**/LIS_HIST*.nc",
                                 output=f"LIS/DA_10km/AMSR2/SURFACEMODEL/LIS_HIST.d01" )
@@ -35,8 +35,10 @@ SNODAS                 = dict(  input=f"mwrzesie/Runs_10km/DA_SNODAS/OUTPUT_snod
                                 output=f"LIS/DA_10km/SNODAS/SURFACEMODEL/LIS_HIST.d01" )
 MCD15A2H_wSmoothing    = dict(  input=f"lahmers/RUN/10km_DOMAIN_DAens20_MCD15A2H.wSmoothing.006_2002_2021/OUTPUT.LAIsmooth.DA/SURFACEMODEL/**/LIS_HIST*.nc",
                                 output=f"LIS/DA_10km/MODIS_wSoother_2002_2021/SURFACEMODEL/LIS_HIST.d01" )
+THYSM                  = dict(  input=f"pliu/WORKING/DA_THYSM/lis_run/OUTPUT_010d/SURFACEMODEL/**/LIS_HIST*.nc",
+                                output=f"LIS/DA_010d/THYSM/SURFACEMODEL/LIS_HIST.d01" )
 
-dset = MCD15A2H_wSmoothing
+dset = THYSM
 
 if __name__ == '__main__':
 
