34a55aacbb0fdd2886ca28c202e78f00d4d5fda4
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-05-28 17:26:59 -0400

    Add catalog write

--- workflows/zarr_conversion.py
+++ workflows/zarr_conversion.py
@@ -17,15 +17,15 @@ if __name__ == '__main__':
     dcm().init_cluster( processes=True )
 
     input_path  = f"{eisc['input_dir']}/{eisc['input_dset']}"
-    output_url = f"file:/{eisc['output_dir']}/{eisc['output_dset']}"
+    output_path = f"{eisc['output_dir']}/{eisc['output_dset']}"
     zarr_url   = f"s3://{eisc['bucket']}/{eisc['output_dset']}"
     cat_name = eisc.get( 'cat_name', eisc['output_dset'].replace("/",".") )
 
-#    zc().standard_conversion( input_path, output_url  )
+#    zc().standard_conversion( input_path, output_path  )
 
-    print( f"S3 upload command:\n\t '>> aws s3 mv {output_url}.zarr {zarr_url}.zarr  --acl bucket-owner-full-control --recursive' ")
+    print( f"S3 upload command:\n\t '>> aws s3 mv {output_path}.zarr {zarr_url}.zarr  --acl bucket-owner-full-control --recursive' ")
 
-    zc().write_catalog( output_url, cat_name )
+    zc().write_catalog( f"{output_path}.zarr", cat_name )
 
     dcm().shutdown()
     sys.exit(0)
