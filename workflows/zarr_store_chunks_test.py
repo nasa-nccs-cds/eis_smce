f3cadd99dd73d79231932c0e3b7003e5d7c9cc10
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-08-17 17:07:48 -0600

    Add chunking zarr test

--- workflows/zarr_store_chunks_test.py
+++ workflows/zarr_store_chunks_test.py
@@ -10,33 +10,42 @@ zarr_store = "/gpfsm/dnb43/projects/p151/zarr/LIS/DELTA_2km/SCENARIO_2/ROUTING/L
 varname = "FloodedArea_tavg"
 
 zds: xa.Dataset = xa.open_zarr( zarr_store )
-variable: Array = zds.data_vars[varname].data
-print( f"Variable Shape: {variable.shape}" )
-print( f"Chunk sizes: {variable.chunksize}" )
-print( f"Chunk Dims: {variable.numblocks}" )
-print( f"Timeslice size: {np.prod(variable.shape[1:])}" )
+xvar: xa.DataArray = zds.data_vars[varname]
+daskvar: Array = xvar.data
+print( f"Variable Shape: {daskvar.shape}" )
+print( f"Chunk sizes: {daskvar.chunksize}" )
+print( f"Chunk Dims: {daskvar.numblocks}" )
+print( f"Timeslice size: {np.prod(daskvar.shape[1:])}" )
 
-i0 = 0
-for i1 in range( variable.numblocks[1] ):
-    for i2 in range(variable.numblocks[2]):
-        chunk: np.ndarray = variable.blocks[i0,i1,i2].compute()
-        num_nan = np.count_nonzero(np.isnan(chunk))
-        print( f"Chunk[{i0},{i1},{i2}]: shape={chunk.shape}, size = {chunk.size}, #NaN: {num_nan}" )
+nnan: np.ndarray = np.count_nonzero( np.isnan( xvar.values ), axis = 0 )
+print( nnan )
 
-print("\n")
-(i0,i1,i2) = ( 0, 2, 3 )
-chunk: np.ndarray = variable.blocks[ i0, i1, i2 ].compute()
-for iS in range( chunk.shape[0] ):
-    cslice = chunk[iS]
-    num_nan = np.count_nonzero(np.isnan(cslice))
-    print(f"Chunk[{i0},{i1},{i2}][{iS}]: shape={cslice.shape}, size = {cslice.size}, #NaN: {num_nan}")
+def count_missing_blocks( variable: Array ):
+    missing_blocks = []
+    for i0 in range( variable.numblocks[0] ):
+        for i1 in range( variable.numblocks[1] ):
+            for i2 in range(variable.numblocks[2]):
+                chunk: np.ndarray = variable.blocks[i0,i1,i2].compute()
+                if not (np.count_nonzero(~np.isnan(chunk)) > 0):
+                    missing_blocks.append( (i0,i1,i2) )
+    print(f"\nMissing Blocks: {missing_blocks}")
 
-missing_blocks = []
-for i0 in range( variable.numblocks[0] ):
+def explore_blocks(  variable: Array  ):
+    i0 = 0
     for i1 in range( variable.numblocks[1] ):
         for i2 in range(variable.numblocks[2]):
             chunk: np.ndarray = variable.blocks[i0,i1,i2].compute()
-            if np.count_nonzero(~np.isnan(chunk)) == 0:
-                missing_blocks.append( (i0,i1,i2) )
+            num_nan = np.count_nonzero(np.isnan(chunk))
+            print( f"Chunk[{i0},{i1},{i2}]: shape={chunk.shape}, size = {chunk.size}, #NaN: {num_nan}" )
 
-print(f"\nMissing Blocks: {missing_blocks}")
+    print("\n")
+    (i0,i1,i2) = ( 0, 2, 3 )
+    chunk: np.ndarray = variable.blocks[ i0, i1, i2 ].compute()
+    for iS in range( chunk.shape[0] ):
+        cslice = chunk[iS]
+        num_nan = np.count_nonzero(np.isnan(cslice))
+        print(f"Chunk[{i0},{i1},{i2}][{iS}]: shape={cslice.shape}, size = {cslice.size}, #NaN: {num_nan}")
+
+
+
+
