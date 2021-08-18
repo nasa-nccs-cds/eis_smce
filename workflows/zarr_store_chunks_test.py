981f2e010bb1d61f3cd2131e9cf72ddd2d05b38b
Thomas Maxwell <thomas.maxwell@nasa.gov>
2021-08-18 14:28:16 -0600

    Add chunking zarr test

--- workflows/zarr_store_chunks_test.py
+++ workflows/zarr_store_chunks_test.py
@@ -6,7 +6,7 @@ from dask.utils import IndexCallable
 logger = logging.getLogger("distributed.utils_perf")
 logger.setLevel(logging.ERROR)
 
-zarr_store = "/gpfsm/dnb43/projects/p151/zarr/LIS/DELTA_2km/SCENARIO_2/ROUTING/LIS_HIST.d01.zarr"
+zarr_store   = "/gpfsm/dnb43/projects/p151/zarr/LIS/DELTA_2km/SCENARIO_2/ROUTING/LIS_HIST.d01.zarr"
 varname = "FloodedArea_tavg"
 
 zds: xa.Dataset = xa.open_zarr( zarr_store )
